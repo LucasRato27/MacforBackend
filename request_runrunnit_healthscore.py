9a538d0244cc9d2568562afc13f1f70fbd0438ca
LucasRato27 <lucas.sampaio.8890@ga.ita.br>
2024-08-12 21:46:22 -0300

    push - bug fix

--- request_runrunnit_healthscore.py
+++ request_runrunnit_healthscore.py
@@ -2,6 +2,9 @@ import requests
 import json
 import pandas as pd
 import datetime
+import gspread
+from gspread_dataframe import set_with_dataframe
+from google.oauth2.service_account import Credentials
 
 def seconds_to_hms(seconds):
     hours = seconds // 3600
@@ -16,7 +19,7 @@ def fetch_runrunit_tasks(
     is_working_on=None,
     sort="desired_date",
     sort_dir="desc",
-    export_filename="tarefas_filtradas",
+    export_filename="tarefas_filtradas"
 ):
     url = "https://runrun.it/api/v1.0/tasks"
     headers = {
@@ -49,15 +52,9 @@ def fetch_runrunit_tasks(
                 all_tasks.extend(tasks)
             else:
                 print(f"Failed to fetch page {page}: {response.status_code}, {response.text}")
-                tasks = response.json()
-                all_tasks.extend(tasks)
                 break
 
         tarefas_filtradas = []
-
-        json_filename = f"tarefas_raw.json"
-        with open(json_filename, "w", encoding='utf-8') as file:
-            json.dump(tarefas_filtradas, file, ensure_ascii=False, indent=4)
 
         for tarefa in all_tasks:
             if tarefa['board_name'].lower() != "acompanhamento de clientes":
@@ -66,7 +63,7 @@ def fetch_runrunit_tasks(
             if estimated_delivery_date:
                 estimated_delivery_date = estimated_delivery_date.split('T')[0]
             else:
-                estimated_delivery_date = 'N/A'  # or any default value
+                estimated_delivery_date = 'N/A'  # or any default value you prefer
 
             tarefa_filtrada = {
                 'id Runrunit': tarefa['id'],
@@ -78,19 +75,17 @@ def fetch_runrunit_tasks(
             }
             tarefas_filtradas.append(tarefa_filtrada)
 
+
         json_filename = f"{export_filename}.json"
         with open(json_filename, "w", encoding='utf-8') as file:
             json.dump(tarefas_filtradas, file, ensure_ascii=False, indent=4)
 
-        # append parameters to the export filename
-        datestring = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")
-        export_filename += f"{datestring}"
-
         # Save to an excel file from pandas
-        excel_filename = f"outputs/{export_filename}{'_closed' if is_closed else ''}.xlsx"
+        excel_filename = f"outputs/{export_filename}.xlsx"
 
         df = pd.DataFrame(tarefas_filtradas)
 
+        # Função para extrair apenas os labels dos campos personalizados
         def extrair_labels(campos_personalizados):
             if isinstance(campos_personalizados, list):
                 return [campo['label'] for campo in campos_personalizados]
@@ -109,8 +104,10 @@ def fetch_runrunit_tasks(
         df = df.rename(columns={
             'custom_26': 'ocorridos',
             'custom_27': 'data',
-            'custom_28': 'cliente'
+            'custom_28': 'cliente',
         })
+
+        df = df.explode('ocorridos')
 
         print("Saving a dataframe with dimensions: ", df.shape)
         df.to_excel(excel_filename, index=False)
@@ -123,32 +120,17 @@ def fetch_runrunit_tasks(
         else:
             return pd.DataFrame()  # return an empty DataFrame if no data was fetched
 
-
     return df
 
 # Call the function with the desired parameters
 df = fetch_runrunit_tasks(
-    pages=5,
+    pages=10,
     limit=1000,
     is_closed=False,
     is_working_on=False,
-    export_filename="feedback_"
+    sort="estimated_delivery_date",
+    sort_dir="desc",
+    export_filename="feedback_full"
 )
 
 print(df.head())  # print the first few rows of the DataFrame to check the result
-
-df_closed = fetch_runrunit_tasks(
-    pages=50,
-    limit=1000,
-    is_closed=True,
-    is_working_on=False,
-    export_filename="feedback_"
-)
-
-print(df_closed.head())  # print the first few rows of the DataFrame to check the result
-
-# save df_full to xlsx file
-
-df_full = pd.concat([df, df_closed])
-
-df_full.to_excel("outputs/tarefas_full.xlsx", index=False)
