952168977958e6c409dc99e0a51552b9df218d20
LucasRato27 <lucas.sampaio.8890@ga.ita.br>
2024-08-25 13:45:44 -0300

    push 2 - added logic to calculo_healthscore.py to sheets upload

--- calculo_healthscore.py
+++ calculo_healthscore.py
@@ -1,5 +1,11 @@
-import pandas as pd
 import numpy as np
+import requests
+import json
+import pandas as pd
+import gspread
+from gspread_dataframe import set_with_dataframe
+from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
 
 def read_google_sheet(sheet_url):
     """
@@ -65,6 +71,37 @@ for i in range(1, len(df)):
 df['Healthscore'] = np.minimum(df['Healthscore'], 10)
 
 
+def upload_to_sheets(df, sheet_name):
+    scopes = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive'
+    ]
+
+    # Configurando as credenciais e autenticando com o Google Sheets
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scopes)
+    client = gspread.authorize(creds)
+
+    # Abrindo a planilha e a aba desejada
+    sheet = client.open_by_url(
+        "https://docs.google.com/spreadsheets/d/1rGiD4Zab5BzY7xJYtnA1GfwqRH4pjlP-i8S-CE5cihs/edit?gid=0#gid=0").sheet1
+
+    try:
+        # Limpar a planilha antes de inserir novos dados
+        sheet.clear()
+
+        # Enviar o DataFrame inteiro para o Google Sheets
+        set_with_dataframe(sheet, df)
+
+        print(f"Data uploaded successfully to Google Sheets: {sheet_name}")
+        return True
+
+    except Exception as e:
+        print(f"An error occurred while uploading to Google Sheets: {e}")
+        return False
+
+upload_to_sheets(df, sheet_name="Macfor")
+
 print(df)
 # save df to excel
+
 df.to_excel("outputs\\healthscore_calculado.xlsx", index=False)
