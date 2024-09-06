ab5f14515f752d2fdbef8989285074507a1c637a
LucasRato27 <lucas.sampaio.8890@ga.ita.br>
2024-09-06 09:31:20 -0300

    Refactor API call and data processing in healthscore script
    Refactored API URL, headers, and parameter handling. Extracted task filtering and custom field processing into separate functions for better modularity. Improved error handling and streamlined Streamlit styling.

--- healthscore_runrunnit_sheets.py
+++ healthscore_runrunnit_sheets.py
@@ -1,13 +1,17 @@
 import requests
-import json
 import pandas as pd
-import gspread
 from gspread_dataframe import set_with_dataframe
 from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
+import gspread
+
+API_URL = "https://runrun.it/api/v1.0/tasks"
+HEADERS = {
+    "App-Key": "8cdb5426d5eb6a8408dec01affa5f023",
+    "User-Token": "1hX3ZnUyCQu5mQtaeCDN"
+}
 
 
-# Função para converter segundos para hms
 def seconds_to_hms(seconds):
     hours = seconds // 3600
     minutes = (seconds % 3600) // 60
@@ -15,8 +19,7 @@ def seconds_to_hms(seconds):
     return f"{int(hours)}:{int(minutes)}:{int(seconds)}"
 
 
-# Função para fazer a chamada de API e retornar df.
-def fetch_runrunit_tasks(
+def fetch_tasks(
         pages=10,
         limit=None,
         is_closed=None,
@@ -24,18 +27,10 @@ def fetch_runrunit_tasks(
         sort=None,
         sort_dir="desc"
 ):
-    url = "https://runrun.it/api/v1.0/tasks"
-    headers = {
-        "App-Key": "8cdb5426d5eb6a8408dec01affa5f023",
-        "User-Token": "1hX3ZnUyCQu5mQtaeCDN"
-    }
-
     all_tasks = []
-
     try:
         for page in range(1, pages + 1):
-            print("opening page", page)
-            parameters = {
+            params = {
                 "limit": limit,
                 "is_closed": is_closed,
                 "sort": sort,
@@ -43,80 +38,47 @@ def fetch_runrunit_tasks(
                 "page": page
             }
             if is_working_on is not None:
-                parameters["is_working_on"] = is_working_on
-
-            response = requests.get(url, headers=headers, params=parameters)
-
+                params["is_working_on"] = is_working_on
+            response = requests.get(API_URL, headers=HEADERS, params=params)
             if response.status_code == 200:
-                tasks = response.json()
-                all_tasks.extend(tasks)
+                all_tasks.extend(response.json())
             else:
                 print(f"Failed to fetch page {page}: {response.status_code}, {response.text}")
                 break
-
-        tarefas_filtradas = []
-
-        for tarefa in all_tasks:
-            if tarefa['board_name'].lower() != "acompanhamento de clientes":
-                continue
-
-            estimated_delivery_date = tarefa.get('estimated_delivery_date')
-            if estimated_delivery_date:
-                estimated_delivery_date = estimated_delivery_date.split('T')[0]
-            else:
-                estimated_delivery_date = 'N/A'  # or any default value
-
-            tarefa_filtrada = {
-                'id Runrunit': tarefa['id'],
-                'titulo': tarefa['title'],
-                'estado': tarefa['state'],
-                'Quadro': tarefa['board_name'],
-                'campos personalizados': tarefa['custom_fields'],
-            }
-            tarefas_filtradas.append(tarefa_filtrada)
-
-        df = pd.DataFrame(tarefas_filtradas)
-
-        # Função para extrair apenas os labels dos campos personalizados
-        # Função para extrair apenas os labels dos campos personalizados
-        def extrair_labels(campos_personalizados):
-            if isinstance(campos_personalizados, list):
-                return [campo['label'] for campo in campos_personalizados]
-            elif isinstance(campos_personalizados, dict):
-                return campos_personalizados.get('label', None)
-            else:
-                return campos_personalizados
-
-        # Aplicar a função a cada coluna de campos personalizados
-        for col in df['campos personalizados'][0].keys():
-            df[col] = df['campos personalizados'].apply(lambda x: extrair_labels(x[col]) if col in x else None)
-
-        # Remover a coluna original de campos personalizados
-        df = df.drop(columns=['campos personalizados'])
-
-        # Renomear as colunas de campos customizáveis
-        df = df.rename(columns={
-            'custom_26': 'ocorridos',
-            'custom_27': 'data',
-            'custom_28': 'cliente',
-        })
-
-        df = df.explode('ocorridos')
-
-        # Substituir NaNs para evitar problemas de formatação
-        df = df.fillna('')
-
-        print("Dataframe fetched with dimensions: ", df.shape)
-
+        return filter_tasks(all_tasks)
     except Exception as e:
         print(f"An error occurred: {e}")
-        if 'tarefas_filtradas' in locals():
-            df = pd.DataFrame(tarefas_filtradas)
-            return df
-        else:
-            return pd.DataFrame()  # return an empty DataFrame if no data was fetched
+        return pd.DataFrame(all_tasks) if all_tasks else pd.DataFrame()
 
-    return df
+
+def filter_tasks(tasks):
+    filtered_tasks = [task for task in tasks if task['board_name'].lower() == "acompanhamento de clientes"]
+    tasks_data = []
+    for task in filtered_tasks:
+        estimated_delivery_date = task.get('estimated_delivery_date', 'N/A').split('T')[0]
+        tasks_data.append({
+            'id Runrunit': task['id'],
+            'titulo': task['title'],
+            'estado': task['state'],
+            'Quadro': task['board_name'],
+            'campos personalizados': task['custom_fields']
+        })
+    df = pd.DataFrame(tasks_data)
+    return process_custom_fields(df)
+
+
+def process_custom_fields(df):
+    def extrair_labels(campos_personalizados):
+        if isinstance(campos_personalizados, list):
+            return [campo['label'] for campo in campos_personalizados]
+        return campos_personalizados.get('label') if isinstance(campos_personalizados, dict) else campos_personalizados
+
+    for col in df['campos personalizados'][0].keys():
+        df[col] = df['campos personalizados'].apply(lambda x: extrair_labels(x[col]) if col in x else None)
+    df.drop(columns=['campos personalizados'], inplace=True)
+    df.rename(columns={'custom_26': 'ocorridos', 'custom_27': 'data', 'custom_28': 'cliente'}, inplace=True)
+    return df.explode('ocorridos').fillna('')
+
 
 def upload_to_sheets(df, sheet_name):
     scopes = [
@@ -130,7 +92,7 @@ def upload_to_sheets(df, sheet_name):
 
     # Abrindo a planilha e a aba desejada
     sheet = client.open_by_url(
-        "https://docs.google.com/spreadsheets/d/1pB8OKJzam3uyRL5y8d2q7ATWKwTliYIjhXFW39uaPAU/edit?gid=0#gid=0").sheet1
+        "https://docs.google.com/spreadsheets/d/1OWdcEc5NozVGXvDkAlrlCgs-5HUc8KeH3BXF-QqHlf4/edit?gid=1456452784#gid=1456452784").sheet1
 
     try:
         # Limpar a planilha antes de inserir novos dados
@@ -140,57 +102,32 @@ def upload_to_sheets(df, sheet_name):
         set_with_dataframe(sheet, df)
 
         print(f"Data uploaded successfully to Google Sheets: {sheet_name}")
+        st.write(f"Data uploaded successfully to Google Sheets: {sheet_name}")
         return True
 
     except Exception as e:
+        st.write(f"An error occurred while uploading to Google Sheets: {e}")
         print(f"An error occurred while uploading to Google Sheets: {e}")
         return False
 
-st.markdown(
-    """
-    <style>
-    div.stButton > button {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        font-size: 80px;
-        padding: 20px 40px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    """
-    <style>
-    .centered-image {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.image("img/Logo.png", width=300, caption="", use_column_width=False, output_format="auto", clamp=False, channels="RGB")
-
-st.title("Runrunit Healthscore Feedback Fetcher")
-
-df = pd.DataFrame()
-
-# Chamar a função para buscar os dados
-if st.button("Executar"):
-    df = fetch_runrunit_tasks(
-        pages=2,
-        sort_dir="desc",
-        sort="id"
+def set_streamlit_styles():
+    st.markdown(
+        """
+        <style>
+        div.stButton > button { display: block; margin-left: auto; margin-right: auto; font-size: 80px; padding: 20px 40px; }
+        .centered-image { display: block; margin-left: auto; margin-right: auto; }
+        </style>
+        """,
+        unsafe_allow_html=True
     )
-    # Fazer o upload dos dados para o Google Sheets
+
+
+set_streamlit_styles()
+st.image("img/Logo.png", width=300)
+st.title("Runrunit Healthscore Feedback Fetcher")
+df = pd.DataFrame()
+if st.button("Executar"):
+    df = fetch_tasks(pages=2, sort_dir="desc", sort="id")
     upload_to_sheets(df, sheet_name="Macfor")
-
-
-# Verificar o DataFrame antes de enviar para o Google Sheets
-print("df head:\n", df.head(), "\n\n")  # Imprime as primeiras linhas do DataFrame para verificar o resultado
-print("df shape:", df.shape)  # Imprime as dimensões do DataFrame para verificar o resultado
+print("df head:\n", df.head(), "\n\n")
+print("df shape:", df.shape)
