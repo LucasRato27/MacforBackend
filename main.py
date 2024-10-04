262fabb6f175be68d6a89017294000e68569e311
Lucas Alencar Sampaio <150623840+LucasRato27@users.noreply.github.com>
2024-10-04 16:35:34 -0300

    alteracao em fetch_runrunnit_tasks 3

--- main.py
+++ main.py
@@ -3,21 +3,42 @@ from fetch_runrunit_tasks import fetch_runrunit_tasks
 from healthscore_runrunnit_sheets import fetch_runrunit_feedback
 from calculo_healthscore import calculate_heathscore
 
+# Estilos de CSS para alterar o fundo para preto e o texto para branco
 st.markdown(
     """
     <style>
+    /* Alterar o fundo para preto e o texto para branco */
+    body {
+        background-color: black;
+        color: white;
+    }
+
+    /* Alterar fundo de outras partes da página */
+    .stApp {
+        background-color: black;
+    }
+
+    /* Estilizando o título e demais textos */
+    h1, h2, h3, h4, h5, h6, p, div, span {
+        color: white !important;
+    }
+
+    /* Personalizando o botão */
     div.stButton > button {
         display: block;
         margin-left: auto;
         margin-right: auto;
         font-size: 80px;
         padding: 20px 40px;
+        background-color: white;
+        color: black;
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
+# Estilo para a imagem centralizada
 st.markdown(
     """
     <style>
