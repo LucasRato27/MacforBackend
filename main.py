23cae3c23cc38a93e3e4c8651f1eee7d89d2b5b2
Lucas Alencar Sampaio <150623840+LucasRato27@users.noreply.github.com>
2024-10-04 13:50:44 -0300

    alteração da main, de novo

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fetch_runrunit_tasks import fetch_runrunit_tasks
 from healthscore_runrunnit_sheets import fetch_runrunit_feedback
 from calculo_healthscore import calculate_heathscore
 
-# Estilos de botão e imagem
 st.markdown(
     """
     <style>
@@ -14,6 +13,14 @@ st.markdown(
         font-size: 80px;
         padding: 20px 40px;
     }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <style>
     .centered-image {
         display: block;
         margin-left: auto;
@@ -24,25 +31,18 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Exibir o logo
 st.image("img/Logo.png", width=300, caption="", use_column_width=False, output_format="auto", clamp=False, channels="RGB")
 
-# Título da aplicação
 st.title("Update Runrunnit Database")
 
-# Input para número de páginas
 n_pags = st.number_input("Número de páginas", min_value=1, max_value=100, value=20, step=1, format="%d")
 
-# Botão para executar as funções
 if st.button("Executar"):
-    try:
-        # Executar as funções
-        fetch_runrunit_tasks(n_pags)
-        fetch_runrunit_feedback()
-        calculate_heathscore()
+    fetch_runrunit_tasks(n_pags)
+    fetch_runrunit_feedback()
+    calculate_heathscore()
 
-        # Exibir mensagem de sucesso
-        st.success("Execução concluída com sucesso!")
-    except Exception as e:
-        # Exibir mensagem de erro em caso de falha
-        st.error(f"Ocorreu um erro durante a execução: {e}")
+
+
+
+
